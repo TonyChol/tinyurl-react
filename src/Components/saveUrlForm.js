cdb65e61f559d9af127b30d4c44f8471342b49a3
Zhibin Cai <yourcaizhibin@gmail.com>
2016-09-27 14:19:49 -0400

    feat(validation): perform validation to the url form

--- src/Components/saveUrlForm.js
+++ src/Components/saveUrlForm.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import '../App.css';
+import { Button } from 'react-bootstrap';
+import { Grid, Row, Col} from 'react-bootstrap';
+import { FormControl, FormGroup, ControlLabel } from 'react-bootstrap';
 
 class SaveUrlForm extends Component {
     constructor(props) {
@@ -8,10 +11,11 @@ class SaveUrlForm extends Component {
             saving: false,
             url: ""
         };
-    }
+    };
     handleInputChange = event => {
        this.setState({
-           url: event.target.value
+           url: event.target.value,
+           saving: false
        });
     };
     handleSave = event => {
@@ -19,12 +23,49 @@ class SaveUrlForm extends Component {
             saving: true
         });
     };
+    getValidationState() {
+        const url = this.state.url;
+        if (this.validUrl(url) === true) {
+            return 'success';
+        }
+        else {
+            return 'error';
+        }
+    };
+    validUrl(str) {
+        let regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+        return regexp.test(str);
+    };
     render() {
         let inputPlaceHolder = "Type the url you want to save...";
         return (
             <div>
-                <input type="text" placeholder={inputPlaceHolder} value={this.state.url} onChange={this.handleInputChange}/>
-                <button className="saveButton" onClick={this.handleSave}>Save</button>
+                <Grid>
+                    <Row className="su-form-group">
+                        <Col xs={12} md={8}>
+                            <form>
+                                <FormGroup
+                                    controlId="formBasicText"
+                                    validationState={this.getValidationState()}
+                                >
+                                    <ControlLabel>{ inputPlaceHolder }</ControlLabel>
+                                    <FormControl
+                                        type="text"
+                                        value={this.state.url}
+                                        placeholder="Enter url"
+                                        onChange={this.handleInputChange}
+                                    />
+                                    <FormControl.Feedback />
+
+                                </FormGroup>
+                            </form>
+                        </Col>
+
+                    </Row>
+                </Grid>
+
+                {/*<button className="saveButton" onClick={this.handleSave}>Save</button>*/}
+                <Button bsStyle="primary" onClick={this.handleSave}>Save</Button>
                 <p>Saving: {this.state.saving.toString()}</p>
                 <p>Url: {this.state.url}</p>
             </div>
